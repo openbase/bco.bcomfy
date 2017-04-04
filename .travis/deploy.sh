2b144bff698a636dabd288bb2a288969f72f01b1
Marian Pohling <mpohling@cit-ec.uni-bielefeld.de>
2017-04-04 15:11:58 +0200

    add travis config

---
+++ .travis/deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo ### deployment disabled
+
+#set -ev
+#echo ### start deployment...
+#mvn deploy -Pdeploy,sonatype --settings .travis/settings.xml -DskipTests=true -B -q -U
+#echo ### deployment successfully finished
