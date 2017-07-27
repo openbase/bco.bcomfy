cff11ac3d047e7721a038f149624c6e540fedec5
Marian Pohling <mpohling@cit-ec.uni-bielefeld.de>
2017-07-27 06:17:46 -0600

    mvn deployment optimized even if disabled.

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -2,7 +2,8 @@
 
 echo ### deployment disabled
 
+#!/bin/bash
 #set -ev
 #echo ### start deployment...
-#mvn deploy -Pdeploy,sonatype --settings .travis/settings.xml -DskipTests=true -B -q -U
+#mvn deploy -Pdeploy,sonatype --settings .travis/settings.xml -DskipTests=true -B
 #echo ### deployment successfully finished
